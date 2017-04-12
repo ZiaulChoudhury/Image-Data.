12402d03d29994c6f40f1909ef368db27236e09b
ZiaulChoudhury <ziaul.choudhury@lc.cuny.edu>
2017-04-12 16:09:22 -0400

    Update ImageProcessingDataScience.py

--- ImageProcessingDataScience.py
+++ ImageProcessingDataScience.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 21 12:27:21 2017
+
 @author: ziaulchoudhury
 """
 
@@ -16,7 +17,7 @@ l2 = []
 l3=[]
 b3=[]
 
-    
+#-----------First Image-------------#        
 img = plt.imread('2015.png')	#Read in image from 2015.png
 plt.imshow(img)			#Load image into matplotlib
 height = img.shape[0]
@@ -32,10 +33,11 @@ for i in range(height):
              b.append(newImage[i,j,2])
 
 plt.imshow(newImage)	#Open window to show image (close to continue)
+plt.title('Image One')
 plt.show() 
 plt.imsave('new2015.png',newImage)
 
-
+#-----------Second Image-------------# 
 img2 = plt.imread('2016.png')	#Read in image from 2016.png
 plt.imshow(img2)			#Load image into matplotlib
 height2 = img2.shape[0]
@@ -51,9 +53,11 @@ for i2 in range(height2):
              b2.append(newImage2[i2,j2,2])
 
 plt.imshow(newImage2)	#Open window to show image (close to continue)
+plt.title('Image Two')
 plt.show() 
 plt.imsave('new2016.png',newImage2)
 
+#-----------Calculations-------------# 
 print('Number of RED Pixes: ',len(b2),', Number of All Pixels: ',len(l2))
 print('Number of Blue Pixes: ',len(b),', Number of All Pixels: ',len(l))
  
@@ -70,5 +74,3 @@ from PIL import Image       # blending imgae to show rivers and flooded area
 background = Image.open("new2016.png")
 foreground = Image.open("new2015.png")
 Image.blend(background,foreground, 0.5).show()
-
-    
